b0a286c9652cc5f7d4faf0dfd1b1f2f4ab38df09
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-08-07 21:32:31 +0530

    07-08-2025 Thu

---
+++ Basic/ex2_swap_two_variables.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter num1 : "))
+num2 = int(input("Enter num2 : "))
+
+print("Before swap : ")
+print("num1 : ",num1," num2 : ",num2)
+
+num1, num2 = num2, num1
+
+print("After swap : ")
+print("num1 : ",num1," num2 : ",num2)
+
