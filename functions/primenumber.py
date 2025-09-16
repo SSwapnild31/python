9a52e00f855cc8b9f162627f81e50c8210186d9b
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-09-16 23:22:27 +0530

    16-09-2025 Tue

---
+++ functions/primenumber.py
@@ -0,0 +1,11 @@
+Starting_value = 1 
+# Taking input from the user 
+n = int(input("Enter the number: ")) 
+print("Prime numbers between", Starting_value, "and", n, "are:") 
+for num in range(Starting_value, n + 1): 
+    if num > 1: 
+        for i in range(2, int(num/2)+1):
+            if (num % i == 0): 
+                break;
+            else: 
+                print(num)
