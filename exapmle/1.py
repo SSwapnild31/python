1a035ca017ea9612638d925664922fef2f32ff62
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-09-21 23:47:21 +0530

    21-09-2025 Sun

---
+++ exapmle/1.py
@@ -0,0 +1,65 @@
+x=5
+y="Ganesh"
+print(x)
+print(y)
+
+x=4
+x="swapnil"
+print(x)
+
+x=str(3)
+y=int(9)
+z=float(3)
+
+x=5
+y="Ganesh"
+z=5.0
+print(type(x))
+print(type(y))
+print(type(z))
+
+if 5>2:
+    print("five is greater")
+
+if 10>5:
+    print("Ten is greater than Five!")
+
+s="Global web solution"
+print(s)
+s="company in jalna"
+print(s)
+
+a={5,2,3,1,4}
+print("a=",a)
+print(type(a))
+
+website="gwsjalna.com"
+print(website)
+
+a,b,c=5,3.2,"Hello"
+print(a)
+print(b)
+print(c)
+
+x=y=z="mss"
+print(x)
+print(y)
+print(z)
+
+print(1,2,3,4)
+print(1,2,3,4,sep='*')
+print(1,2,3,4,sep='#',end='8')
+
+print("")
+x=15
+y=4
+print('x+y=',x+y)
+print('x-y=',x-y)
+print('x*y=',x*y)
+print('x/y=',x/y)
+print('x>y is',x>y)
+print('x<y is',x<y)
+print('x==y is',x==y)
+print('x!=y is',x!=y)
+print('x>=y is',x>=y)
+print('x<=y is',x<=y)
