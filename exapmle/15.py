694f4373c8bf2123b5fcec46539f8f4d689b5cbe
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-09-26 22:21:39 +0530

    26-09-2025 Fri

---
+++ exapmle/15.py
@@ -0,0 +1,16 @@
+#1
+thislist = ["apple","banana","cherry"]
+print(thislist)
+
+#2
+thislist = ["apple","banana","cherry"]
+print(len(thislist))
+
+#3
+list1 = ["apple","banana","cherry"]
+list2 = [1,5,7,9,3]
+list3 = [True,False,False]
+
+print(list1)
+print(list2)
+print(list3)
