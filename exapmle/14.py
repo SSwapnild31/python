f31c1a80cc7271e6f2f2fc878b8b1a7c9939130a
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-09-26 22:21:19 +0530

    26-09-2025 Fri

---
+++ exapmle/14.py
@@ -0,0 +1,31 @@
+#first
+a = "hello world!"
+print(a.upper())
+
+#second
+a = "HELLO WORLD!"
+print(a.lower())
+
+#third
+a = "Hello World!"
+print(a.strip())#return the value of variable
+
+#fourth
+a = "Hello World!"
+print(a.replace("H","J"))
+
+#fifth
+a = "Hello World!"
+print(a.split(",")) #return ['Hello','World!']
+
+#sixth
+a = "Hello"
+b = "World!"
+c= a + b
+print(c)
+
+#seven
+age = 21
+txt = "My name is swapnil, and I am {} year young"
+print(txt.format(age))
+
