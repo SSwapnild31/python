7452dab0e6d9a8cc932ca7eca192f1fe969a6db9
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-10-01 23:52:42 +0530

    01-10-2025 wed

---
+++ exapmle/18.py
@@ -0,0 +1,9 @@
+#1
+thislist = ["banana","Orange","Kiwi","cherry"]
+thislist.sort(key = str.lower)
+print(thislist)
+
+#2
+thislist = ["banana","Orange","Kiwi","cherry"]
+thislist.reverse();
+print(thislist)
