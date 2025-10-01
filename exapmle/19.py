90179a8599164557c3c67a80286f5b359c580af5
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-10-01 23:53:04 +0530

    01-10-2025 wed

---
+++ exapmle/19.py
@@ -0,0 +1,11 @@
+#1
+thisset = {"apple","banana","cherry"}
+print(thisset)
+
+#2
+thisset = {"apple","banana","cherry","apple"}
+print(thisset)
+
+#3
+thisset = {"apple","banana","cherry"}
+print(len(thisset))
