1570cc3dc45cd66c30394adabf815ffc68328bb0
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-10-03 23:04:26 +0530

    03-10-2025 Fri

---
+++ exapmle/22.py
@@ -0,0 +1,9 @@
+x={"apple","banana","cherry"}
+y={"google","microsoft","apple"}
+x.symmetric_difference_update(y)
+print(x)
+    
+x={"apple","banana","cherry"}
+y={"google","microsoft","apple"}
+z=x.symmetric_difference(y)
+print(z)
